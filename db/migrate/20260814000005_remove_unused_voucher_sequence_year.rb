fd94039bae4caa9be9063e199cb24c910813fbea
Maloma Thado <zlwmpthado@gmail.com>
2026-09-26 00:11:05 +0900

    Make voucher cleanup migration deploy-safe

--- db/migrate/20260814000005_remove_unused_voucher_sequence_year.rb
+++ db/migrate/20260814000005_remove_unused_voucher_sequence_year.rb
@@ -3,10 +3,10 @@ class RemoveUnusedVoucherSequenceYear < ActiveRecord::Migration[8.1]
   # finance_voucher_sequences table (see CreateFinanceVoucherSequences).
   # Never used by any code path.
   def up
-    remove_column :finance_units, :voucher_sequence_year, :integer
+    remove_column :finance_units, :voucher_sequence_year, :integer if column_exists?(:finance_units, :voucher_sequence_year)
   end
 
   def down
-    add_column :finance_units, :voucher_sequence_year, :integer
+    add_column :finance_units, :voucher_sequence_year, :integer unless column_exists?(:finance_units, :voucher_sequence_year)
   end
 end
